facd4943cfb17426245df29f9a70d17cc7beb093
keksik77 <lukashevich7tim@gmail.com>
2020-05-17 23:25:12 +0300

    feat: completed cssBuilder task

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -115,32 +115,124 @@ function fromJSON(proto, json) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    if (this.elValue) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    if (this.idValue) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+      );
+    }
+    const copy = { ...this };
+
+    if (!copy.elValue) copy.elValue = value;
+    else copy.elValue += value;
+
+    return copy;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    if (this.idValue) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    if (this.classValue || this.pEValue) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+      );
+    }
+    const copy = { ...this };
+
+    if (!copy.idValue) copy.idValue = `#${value}`;
+    else copy.idValue += `#${value}`;
+
+    return copy;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    if (this.attrValue) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+      );
+    }
+    const copy = { ...this };
+
+    if (!copy.classValue) copy.classValue = `.${value}`;
+    else copy.classValue += `.${value}`;
+
+    return copy;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    if (this.pCValue) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+      );
+    }
+    const copy = { ...this };
+
+    if (!copy.attrValue) copy.attrValue = `[${value}]`;
+    else copy.attrValue += `[${value}]`;
+
+    return copy;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    if (this.pEValue) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+      );
+    }
+    const copy = { ...this };
+
+    if (!copy.pCValue) copy.pCValue = `:${value}`;
+    else copy.pCValue += `:${value}`;
+
+    return copy;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    if (this.pEValue) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    const copy = { ...this };
+
+    if (!copy.pEValue) copy.pEValue = `::${value}`;
+    else copy.pEValue += `::${value}`;
+
+    return copy;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    const copy = { ...this };
+
+    const value = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+
+    if (!copy.value) copy.value = value;
+    else copy.value += value;
+
+    return copy;
+  },
+
+  stringify() {
+    if (this.value) return this.value;
+
+    let value = '';
+
+    if (this.elValue) value += this.elValue;
+    if (this.idValue) value += this.idValue;
+    if (this.classValue) value += this.classValue;
+    if (this.attrValue) value += this.attrValue;
+    if (this.pCValue) value += this.pCValue;
+    if (this.pEValue) value += this.pEValue;
+
+    return value;
   },
 };
 
